0625df40475a16c7dd6e3fa270bda4846c4c57c9
henryidk <henryalibat4@gmail.com>
2026-05-11 03:21:00 -0600

    refactor: eliminar ScheduleModule.forRoot() duplicado en CleanupModule

--- backend/src/cleanup/cleanup.module.ts
+++ backend/src/cleanup/cleanup.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { CleanupService } from './cleanup.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
   providers: [CleanupService],
 })
 export class CleanupModule {}

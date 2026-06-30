1c8f400cb30359cee627803c0a58f6f30d86b8b7
henryidk <henryalibat4@gmail.com>
2026-06-30 02:00:24 -0600

    chore: bump version a v2.3.0

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -204,7 +204,7 @@ export default function Login() {
           <p className="text-slate-400 text-xs">
             Sistema protegido • Acceso autorizado unicamente
           </p>
-          <p className="text-slate-300 text-xs mt-2">v2.1.0</p>
+          <p className="text-slate-300 text-xs mt-2">v2.3.0</p>
         </div>
       </div>
     </div>
